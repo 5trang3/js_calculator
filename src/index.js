e1a45edfb5d94c2fc05cd28562f70d2bae442724
Prasant Prasath <33533156+5trang3@users.noreply.github.com>
2020-01-08 15:23:50 -0800

    Add constant array of button info and pass into Calculator as props.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+let buttonInfo = {
+  7 : 'seven',
+  8 : 'eight',
+  9 : 'nine',
+  '+' : 'add',
+  'AC' : 'clear',
+  4 : 'four',
+  5 : 'five',
+  6 : 'six',
+  '-' : 'minus',
+  1 : 'one',
+  2 : 'two',
+  3 : 'three',
+  '/' : 'divide',
+  '=' : 'equals',
+  '.' : 'decimal',
+  0 : 'zero',
+  'x' : 'multiply'
+}
+
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
@@ -37,4 +57,4 @@ class Button extends React.Component {
   }
 }
 
-ReactDOM.render(<Calculator/>, document.getElementById('root'))
+ReactDOM.render(<Calculator buttonInfo={buttonInfo}/>, document.getElementById('root'))
